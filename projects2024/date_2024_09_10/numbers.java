a38210d9d4adfa341d2cff77ea5ebec3a3a51b27
daxyman <danielfelipegg3005@gmail.com>
2024-09-17 11:12:19 -0400

    im tired brah

---
+++ projects2024/date_2024_09_10/numbers.java
@@ -0,0 +1,5 @@
+package projects2024.java projects.date_2024_09_10;
+
+public class numbers {
+    
+}
--- projects2024/java projects/date_2024_09_17/trace2/test.java
+++
@@ -1,5 +0,0 @@
-package projects2024.java.projects.2024 - 09 - 17.trace2;
-
-public class test {
-    
-}
