f35e4a614e358a49bfa2c72cb37738dda2f1edc1
daxyman <danielfelipegg3005@gmail.com>
2024-09-26 10:41:56 -0400

    ex 4 and 5

---
+++ projects2024/exercices/exercise_5.java
@@ -0,0 +1,24 @@
+package exercices;
+
+import java.util.Scanner;
+
+public class exercise_5 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine();
+        
+        int number1 = Integer.parseInt(input1);
+        if (number1 >=90) {
+            System.out.println("A");
+        }else if (number1 >= 80) {
+            System.out.println("B");
+        }else if (number1 >= 70) {
+            System.out.println("C");
+        }else if (number1 >= 60) {
+            System.out.println("D");
+        }else if (number1 < 60) {
+            System.out.println("F");
+        }
+        scanner.close();
+    }
+}
