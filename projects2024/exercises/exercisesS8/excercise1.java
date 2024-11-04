48d216e1afce53cc3e2b96334a7e61f357caf141
daxyman <danielfelipegg3005@gmail.com>
2024-11-03 21:42:34 -0500

    jh

--- projects2024/exercises/exercisesS8/excercise1.java
+++ projects2024/exercises/exercisesS8/excercise1.java
@@ -2,30 +2,65 @@ package exercisesS8;
 /*1. Calculatrice simple
 Objectif : Créer des méthodes pour les opérations de base (addition, soustraction, multiplication, division, puissance).
 
-•	Exemple : int addition(int a, int b)
+•	Exemple : double addition(double a, double b)
  */
 import java.util.Scanner;
 public class excercise1 {
 
     public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    double a = scanner.nextDouble();
+    double b = scanner.nextDouble();
+    scanner.close();
+    int option = scanner.nextInt();
+    switch (option) {
+        case 1:
+            somme();
+            break;
+    
+        case 2:
+            
+            break;
+    
+        case 3:
+            
+            break;
+    
+        case 4:
+            
+            break;
+    
+        default:
+            break;
+    }
 
+    }
+    public static double somme(double a, double b){
+        double result = a + b;
+        System.out.println(result);
+        return result;
+    }
+    public static double soustraction(double a, double b){
+
+        double result = a - b;
+        return result;
+    }
+    public static double division(double a, double b){
+        double result = a / b;
+        return result;
+    }
+    public static double multiplication(double a, double b){
+        double result = a * b;
+        return result;
+    }
+    public static double Puissance(double a, double b){
+        double result = a + b; //a times a, the amount of times b says is so, a loop?
+        for (double i=0 ;i<b; i++){
+            System.out.println(i * a);
+        }
+        return result;
 
     }
-    public static int somme(int a, int b){
-        int result = a + b;
-        return result;
-    }
-    public static int soustraction(int a, int b){
-        int result = a + b;
-        return result;
-    }
-    public static int division(int a, int b){
-
-        int a2 = Double.parseDouble();
-        return result;
-    }
-    public static int multiplication(int a, int b){
-        int result = a + b;
-        return result;
-    }
+    
+    
 }
