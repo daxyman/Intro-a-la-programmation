2f31959ad08aea7d2b315fd11323516a8be1c29f
daxyman <danielfelipegg3005@gmail.com>
2024-11-04 20:05:39 -0500

    ex1

--- projects2024/exercises/exercisesS8/excercise1.java
+++ projects2024/exercises/exercisesS8/excercise1.java
@@ -7,60 +7,79 @@ Objectif : Créer des méthodes pour les opérations de base (addition, soustrac
 import java.util.Scanner;
 public class excercise1 {
 
-    public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    double a = scanner.nextDouble();
-    double b = scanner.nextDouble();
-    scanner.close();
-    int option = scanner.nextInt();
-    switch (option) {
-        case 1:
-            somme();
-            break;
-    
-        case 2:
-            
-            break;
-    
-        case 3:
-            
-            break;
-    
-        case 4:
-            
-            break;
-    
-        default:
-            break;
-    }
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+System.out.println("number a");
+double a = scanner.nextDouble();
+System.out.println("number b");
+double b = scanner.nextDouble(); 
+System.out.println("choisisez");
+System.out.println("1 somme");
+System.out.println("2 soustraction ");
+System.out.println("3 division ");
+System.out.println("4 multiplication");
+System.out.println("5 puissance");
+int option = scanner.nextInt();
+scanner.close();
+switch (option) {
+case 1:
+    somme(a, b);
+    break;
 
-    }
-    public static double somme(double a, double b){
-        double result = a + b;
-        System.out.println(result);
-        return result;
-    }
-    public static double soustraction(double a, double b){
+case 2:
+    soustraction(a, b);
+    break;
+    
+case 3:
+    division(a, b);
+    break;
 
-        double result = a - b;
-        return result;
-    }
-    public static double division(double a, double b){
-        double result = a / b;
-        return result;
-    }
-    public static double multiplication(double a, double b){
-        double result = a * b;
-        return result;
-    }
-    public static double Puissance(double a, double b){
-        double result = a + b; //a times a, the amount of times b says is so, a loop?
-        for (double i=0 ;i<b; i++){
-            System.out.println(i * a);
-        }
-        return result;
+case 4:
+    multiplication(a, b);
+    break;
 
+case 5:
+    puissance(a, b);
+    break;
+
+default:
+    break;
+}
+
+}
+public static double somme(double a, double b){
+    double result = (a + b);
+    System.out.println(result);
+    return result;
+}
+public static double soustraction(double a, double b){
+
+    double result = (a - b);
+    System.out.println(result);
+    return result;
+}
+public static double division(double a, double b){
+    double result = (a / b);
+    System.out.println(result);
+    return result;
+}
+public static double multiplication(double a, double b){
+    double result = (a * b);
+    System.out.println(result);
+    return result;
+}
+public static double puissance(double a, double b){
+    //a times a, the amount of times b says is so, a loop?
+    //a = number to multiply
+    //b = number of times something will happen (a will multiply themself/eves?)
+    //a^b = a*a .. b amount of times
+    double result = 1;
+    for(double i=0; i< b;i++){
+        result *= a;
     }
+    System.out.println(result);
+    return result;
+}
     
     
 }
