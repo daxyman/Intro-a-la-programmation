d14cf0f97ebe4a6136bfda21878fd7334dbf35cb
daxyman <danielfelipegg3005@gmail.com>
2024-12-18 14:56:55 -0500

    sdfghj

--- projects2024/finalExamPrep/finalExam.java
+++ projects2024/finalExamPrep/finalExam.java
@@ -14,13 +14,11 @@ public class finalExam {
          * Stringbuilder
          * 
          */
-        Scanner scanner = new Scanner(System.in);
-        subjects(scanner);
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            subjects(scanner);
 
-    }
-    public static String content(){
-        String text = "hello!";
-        return text;
+        }
     }
     public static void subjects(Scanner scanner) {
         System.out.println("there are 2 things you need to focus on");
@@ -31,6 +29,7 @@ public class finalExam {
         picker(choice);
     }
     public static void picker (int choice){
+        try{
         switch(choice){
         /* vsc proposed this
          * case 1 -> howStringBuilderWorks();
@@ -38,17 +37,42 @@ public class finalExam {
          * i didnt know you could write switch like this tbh
          */
             case 1:
-            System.out.println(howStringBuilderWorks());
+            howStringBuilderWorks();
             break;
             case 2:
             System.out.println(howjavaFileWorks());
             break;
         }
-
     }
-    public static String howStringBuilderWorks(){
-        String descriptionStrBuld = "StringBuilder in Java represents a mutable sequence of characters.\nBut what even is a \"mutable sequence of characters\" you migth ask?";
-        return(descriptionStrBuld);
+    catch(Exception e){
+        System.out.println("dumbass");
+        picker(choice);
+    }
+    }
+    public static void howStringBuilderWorks(){
+        System.out.println("\nStringBuilder in Java represents a mutable sequence of characters.\n\nBut what even is a \"mutable sequence of characters\" you migth ask? \n\n it is actually very simple, a mutable sequence of characters allows you to modify the sequence of characters without creating a new object to make a change\n");
+        
+        System.out.println("We can see here how unmutable sequences of characters normally work:\n");
+        String str ="hello";
+        System.out.println("original String: " + str);
+
+        //concatenating "word" to the string "str"
+
+        String modifiedStr = str + " world";
+        System.out.print ("modified String: " + modifiedStr);
+
+        //keeps the reference
+        System.out.println("\nOriginal String reference remains the same: " + str + "\n");
+
+        System.out.println("And now we can see how mutable sequences of characters would work (StringBuilder)\n");
+        StringBuilder name = new StringBuilder("hello");
+        System.out.println("original StringBuilder: " + name +"\n");
+        //append the word "world" to stringbuilder
+        name.append(" world");
+        System.err.println("Modified Stringbuilder: " + name);
+        //see the result
+        System.out.println("StringBuilder reference is now changed " + name + "\n");
+        System.err.println("When you modify a StringBuilder (e.g., using .append(), .insert(), .delete(), etc.), the changes happen directly to the same StringBuilder object. No new object is created. This is what makes StringBuilder a mutable sequence of characters.");
     }
     public static String howjavaFileWorks(){
         String descriptionjavFile = "d";
