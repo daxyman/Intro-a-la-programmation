07a8e6810086b6733a7170026bc2417c7db33b1c
daxyman <danielfelipegg3005@gmail.com>
2024-12-18 22:49:02 -0500

    try-catch

---
+++ projects2024/personal_notes/tryCatch/howtoTryCatch.java
@@ -0,0 +1,11 @@
+package personal_notes.tryCatch;
+import java.util.Scanner;
+public class howtoTryCatch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        int divident = scanner.nextInt();
+        int divisor = scanner.nextInt();
+        System.out.println(divident/divisor);
+    }
+}
